7e73bbdf09afe64114af628a0c697977031f61ed
mark-elias <mark7elias@gmail.com>
2024-06-01 12:32:54 -0700

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import "./styles.css"
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
