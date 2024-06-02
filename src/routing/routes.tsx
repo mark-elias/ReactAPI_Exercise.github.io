ff9406ea2d47988a8b1d745e7f194ae6c45e0a68
mark-elias <mark7elias@gmail.com>
2024-06-01 21:33:56 -0700

    PAST DUE - just trying to fix my error

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter(
     },
   ],
 
-  { basename: "https://github.com/mark-elias/ReactAPI_Exercise.github.io" }
+  { basename: "https://github.com/mark-elias/ReactAPI_Exercise.github.io/blob/main/src/routing/HomePage.tsx" }
 );
 
 export default router;
